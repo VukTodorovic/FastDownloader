fe041870d3e871170ab76944cbe0f917b7cb6f41
VukTodorovic <vt.todorovic@gmail.com>
2023-01-17 04:01:17 +0100

    Sending over multiple threads finally working concurently but without k prefix

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
-response = 'ACCEPTED 7 291693 54565 62468 52116 59646 52602 52603 52604'
 
-responseValues = response.split(' ')
+file = open('./server/files/gamefile.dll', 'rb')
 
-streamPorts = []
-for i in range(3, 3+int(responseValues[1])):
-    print(f'i: {i}')
-    streamPorts.append(int(responseValues[i]))
+chunk = file.read(1000)
 
-print(f'response: {response}\nresponseValues: {responseValues}\nstreamPorts: {streamPorts}')
+file.close()
+
+output = str(3).encode() + b' ' + chunk
+
+print(output)
