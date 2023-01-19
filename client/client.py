af51cea4500359fa0e094b22d033808e28a2a4a0
VukTodorovic <vt.todorovic@gmail.com>
2023-01-19 05:22:12 +0100

    Project finished, V1.0

--- client/client.py
+++ client/client.py
@@ -6,8 +6,6 @@ import time
 from queue import Queue
 
 DEFAULT_BUFLEN = 1400
-mutexFile = threading.Lock()
-mutexConn = threading.Lock()
 
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -45,11 +43,12 @@ for i in range(3, 3+int(responseValues[1])):
     streamPorts.append(int(responseValues[i]))
 
 
-downloadedFile = open('./files/' + fileName, 'wb')
+# downloadedFile = open('./files/' + fileName, 'wb')
 chunksRecieved = 0
-nextChunkToWrite = 0
+# nextChunkToWrite = 0
 finishedReceiving = False
 chunkQueue = []
+progress = 0
 
 # testing
 fileWrites = 0
@@ -60,57 +59,49 @@ firstElemTest = 0
 lastWritten = 0
 
 # Function for file write
-def writeBytesToFile(bytesToWrite):
-    mutexFile.acquire()
-    global fileWrites
-    fileWrites += 1
-    global nextChunkToWrite
-    
-    downloadedFile.write(bytesToWrite)
-    nextChunkToWrite += 1
+# def writeBytesToFile(bytesToWrite):
+#     downloadedFile.write(bytesToWrite)
 
-    mutexFile.release()
 
 # Function for inserting new chunk in sorted order
 def linear_search_insert(arr, orderNum):
-    global linearInserts
-    linearInserts += 1
     for i in range(len(arr)):
         try:
             currentItem = arr[i]
         except:
             i=0
             currentItem = arr[i]
+            print('Error')
         if currentItem[0] > orderNum:
             return i
     return len(arr)
 
 # Testing
 def printCurrentProgress():
-    print('Progress:')
     while not finishedReceiving:
-        print('\n\n--------------------------')
-        print(f'fileWrites: {fileWrites} & linearInserts: {linearInserts}')
-        print('--------------------------')
-        print(f'Puta usao u drugi if: {insertedFromQueue}')
-        print('--------------------------')
-        print(f'Queue size: {len(chunkQueue)}')
-        print('--------------------------')
-        print(f'firstElemTest: {firstElemTest}')
-        print('--------------------------')
-        print(f'Next chunk to write: {nextChunkToWrite}')
-        print('--------------------------')
-        print(f'Chunks expected: {k}')
-        print('--------------------------')
-        print(f'Chunks received: {chunksRecieved}')
-        print('--------------------------')
-        print('Queue:')
-        for i in range(len(chunkQueue)):
-            cq = chunkQueue[i]
-            print(cq[0])
-            if i == 10:
-                break
-        time.sleep(5)  # Wait for 5 seconds
+        # print('\n\n--------------------------')
+        # print(f'fileWrites: {fileWrites} & linearInserts: {linearInserts}')
+        # print('--------------------------')
+        # print(f'Puta usao u drugi if: {insertedFromQueue}')
+        # print('--------------------------')
+        # print(f'Queue size: {len(chunkQueue)}')
+        # print('--------------------------')
+        # print(f'firstElemTest: {firstElemTest}')
+        # print('--------------------------')
+        # print(f'Next chunk to write: {nextChunkToWrite}')
+        # print('--------------------------')
+        # print(f'Chunks expected: {k}')
+        # print('--------------------------')
+        # print(f'Chunks received: {chunksRecieved}')
+        # print('--------------------------')
+        # print('Queue:')
+        # for i in range(len(chunkQueue)):
+        #     cq = chunkQueue[i]
+        #     print(cq[0])
+        #     if i == 10:
+        #         break
+        print(f'{progress}%')
+        time.sleep(1)  # Wait for 5 seconds
     
 
 # Funtion for thread
@@ -119,7 +110,8 @@ k = int(responseValues[2])
 def connectToStream(portNumber):
     global chunksRecieved
     global finishedReceiving
-    global kill
+    # global progress
+    # global kill
 
     # Create a TCP/IP socket and connect to the stream
     clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -128,57 +120,57 @@ def connectToStream(portNumber):
     print(f'Connected to stream on port: {portNumber}')
 
     # Client gets chunks of data and writes them to a file
-    while not finishedReceiving:
+    while chunksRecieved < k:
         resp = clientSocket.recv(DEFAULT_BUFLEN)
         if resp == b'':
-            finishedReceiving = True
-            kill = True
+            # finishedReceiving = True
+            # kill = True
             sys.exit()
+
         response_parts = resp.split(b'$SEPARATOR$') 
-        try:
-            ordinalNumber = int(response_parts[0].decode('utf-8'))
-        except:
-            print(response_parts[0])
+        ordinalNumber = int(response_parts[0].decode('utf-8'))
         chunk = response_parts[1]
-        # print('----------------')
-        # print(response_parts)
-        # print('----------------')
 
-        if nextChunkToWrite == ordinalNumber:
-            writeBytesToFile(chunk)
-        else:
-            posInQueue = linear_search_insert(chunkQueue, ordinalNumber)
-            values = [ordinalNumber, chunk]
+        # if nextChunkToWrite == ordinalNumber:
+        #     writeBytesToFile(chunk)
+        # else:
+        #     posInQueue = linear_search_insert(chunkQueue, ordinalNumber)
+        #     values = [ordinalNumber, chunk]
 
-            chunkQueue.insert(posInQueue, values)
+        #     chunkQueue.insert(posInQueue, values)
+
+        # pos = linear_search_insert(chunkQueue, ordinalNumber)
+        values = [ordinalNumber, chunk]
+        chunkQueue.append(values)
+        # chunkQueue.insert(pos, values)
         
         chunksRecieved += 1
-        if chunksRecieved >= k:
-            finishedReceiving = True
+        # progress = chunksRecieved // k
 
 
+    finishedReceiving = True
     clientSocket.close() #testing
-    print(f'Socket on port {portNumber} has finished receiving')
-    print('--------------------------')
-    print(f'fileWrites: {fileWrites}')
-    print('--------------------------')
-    print(f'linearInserts: {linearInserts}')
+    # print(f'Socket on port {portNumber} has finished receiving')
+    # print('--------------------------')
+    # print(f'fileWrites: {fileWrites}')
+    # print('--------------------------')
+    # print(f'linearInserts: {linearInserts}')
     
 
-def insertFromQueue():
-    global insertedFromQueue
-    global firstElemTest
+# def insertFromQueue():
+#     global insertedFromQueue
+#     global firstElemTest
 
-    print('Usao u insertFromQueue()')
-    while (not finishedReceiving or not len(chunkQueue)==0) and kill==False:
-        if not len(chunkQueue)==0:
-            firstElem = chunkQueue[0]
-            if firstElem[0] == nextChunkToWrite:
-                insertedFromQueue += 1
-                writeBytesToFile(firstElem[1])
-                chunkQueue.pop(0)
-            else:
-                firstElemTest = firstElem[0]
+#     print('Usao u insertFromQueue()')
+#     while (not finishedReceiving or not len(chunkQueue)==0) and kill==False:
+#         if not len(chunkQueue)==0:
+#             firstElem = chunkQueue[0]
+#             if firstElem[0] == nextChunkToWrite:
+#                 insertedFromQueue += 1
+#                 writeBytesToFile(firstElem[1])
+#                 chunkQueue.pop(0)
+#             else:
+#                 firstElemTest = firstElem[0]
 
 
 # Calling threads
@@ -189,21 +181,21 @@ for streamPort in streamPorts:
     transfer_thread.start()
     transferThreads.append(transfer_thread)
 
-queueThread = threading.Thread(target=insertFromQueue)
-queueThread.start()
+# queueThread = threading.Thread(target=insertFromQueue)
+# queueThread.start()
 
 
 # progressThread = threading.Thread(target=printCurrentProgress)
 # progressThread.start()
 # progressThread.join()
 
-queueThread.join()
+# queueThread.join()
 
 # Join threads
 for tr_thread in transferThreads:
     tr_thread.join()
 
-downloadedFile.close()
+# downloadedFile.close()
 
 # Writing k ordinals to a file
 # ordinalsFile = open('ordinals.txt', 'w')
@@ -213,14 +205,38 @@ downloadedFile.close()
 
 # ordinalsFile.close()
 
-
-
 transferTime = datetime.now() - startTime
+print('Done downloading\nWriting to a file...')
+
+# Writing chunks to a file
+downloadedFile = open('./files/' + fileName, 'wb')
+
+nextChunkToWrite = 0
+# counter = 0
+while not len(chunkQueue) == 0:
+    for i in range(len(chunkQueue)):
+        item = chunkQueue[i]
+        if item[0] == nextChunkToWrite:
+            downloadedFile.write(item[1])
+            chunkQueue.pop(i)
+            break
+    nextChunkToWrite += 1
+    # item = chunkQueue[counter]
+    # downloadedFile.write(item[counter])
+    # chunkQueue.pop(counter)
+    # counter += 1
+
+downloadedFile.close()
+
+
+
+
+print(f'File "{fileName}" created!')
 print('--------------------------------')
 print(f'Transfer time: {transferTime}')
 print(f'Chunks expected: {k}')
 print(f'Chunks received: {chunksRecieved}')
-print(f'File size: {(chunksRecieved/1024)} KB')
+print(f'File size: {(chunksRecieved/1024)} MB')
 
 
 # Close the connection
